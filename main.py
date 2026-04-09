cfbf36d0109805a853d6557462beb232aee6c001
T1moooon <t1moooong@gmail.com>
2026-04-09 23:00:40 +0300

    add star

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ import time
 
 def draw(canvas):
     row, column = (5, 20)
-    canvas.addstr(row, column, "Hello, World!")
     canvas.border()
-    canvas.refresh()
-    time.sleep(5)
+    curses.curs_set(0)
+    while True:
+        canvas.addstr(row, column, "*", curses.A_DIM)
+        time.sleep(2)
+        canvas.refresh()
+        canvas.addstr(row, column, "*")
+        time.sleep(0.3)
+        canvas.refresh()
+        canvas.addstr(row, column, "*", curses.A_BOLD)
+        time.sleep(0.5)
+        canvas.refresh()
+        canvas.addstr(row, column, "*")
+        time.sleep(0.3)
+        canvas.refresh()
 
 
 if __name__ == "__main__":
