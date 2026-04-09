86b522c0cd8ede61995d3d3e917d83a2ba3297c4
T1moooon <t1moooong@gmail.com>
2026-04-09 22:41:05 +0300

    add cursed example

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import curses
+import time
+
+
+def draw(canvas):
+    row, column = (5, 20)
+    canvas.addstr(row, column, "Hello, World!")
+    canvas.refresh()
+    time.sleep(5)
+
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    curses.update_lines_cols()
+    curses.wrapper(draw)
