4edf3680a16dde1236ecd38f3e1d00bddf3591e6
T1moooon <t1moooong@gmail.com>
2026-04-27 16:46:11 +0300

    use async func

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
+import asyncio
 import curses
 import time
 
 
-def draw(canvas):
-    row, column = (5, 20)
-    canvas.border()
-    curses.curs_set(0)
+async def blink(canvas, row, column, symbol="*"):
     while True:
-        canvas.addstr(row, column, "*", curses.A_DIM)
-        time.sleep(2)
-        canvas.refresh()
-        canvas.addstr(row, column, "*")
-        time.sleep(0.3)
-        canvas.refresh()
-        canvas.addstr(row, column, "*", curses.A_BOLD)
-        time.sleep(0.5)
-        canvas.refresh()
-        canvas.addstr(row, column, "*")
-        time.sleep(0.3)
-        canvas.refresh()
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+
+def draw(canvas):
+    curses.curs_set(0)
+    canvas.border()
+
+    coroutine = blink(canvas, 5, 20)
+
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(2)
+
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(0.3)
+
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(0.5)
+
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(0.3)
+    time.sleep(3)
 
 
 if __name__ == "__main__":
