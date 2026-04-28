04c45d87f89d595d63bb4ab76c67f805916a1026
T1moooon <t1moooong@gmail.com>
2026-04-28 19:20:23 +0300

    add shot animation

--- main.py
+++ main.py
@@ -7,6 +7,36 @@ import time
 TIC_TIMEOUT = 0.1
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), "*")
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), "O")
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), " ")
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = "-" if columns_speed else "|"
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), " ")
+        row += rows_speed
+        column += columns_speed
+
+
 async def blink(canvas, row, column, symbol="*"):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
@@ -38,11 +68,18 @@ def draw(canvas):
     ]
 
     coroutines = [blink(canvas, row, col, random.choice("+*.:")) for row, col in coords]
+    coroutines.append(fire(canvas, height - 2, width // 2))
+
+    for coro in coroutines:
+        for _ in range(random.randint(0, 30)):
+            coro.send(None)
 
     while True:
-        for corotine in coroutines:
-            for _ in range(random.randint(0, 30)):
-                corotine.send(None)
+        for coroutine in coroutines[:]:
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.remove(coroutine)
         canvas.refresh()
         time.sleep(TIC_TIMEOUT)
 
