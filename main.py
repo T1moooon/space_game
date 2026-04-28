fe6ae69d8f44cf85956cf17e7f287ea5c321853c
T1moooon <t1moooong@gmail.com>
2026-04-28 22:20:14 +0300

    add rocket

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import curses
 import random
 import time
 
+from cursed_tools import draw_frame, get_frame_size
+
 
 TIC_TIMEOUT = 0.1
 
@@ -56,11 +58,30 @@ async def blink(canvas, row, column, symbol="*"):
             await asyncio.sleep(0)
 
 
+async def animate_rocket(canvas, row, column, frame_1, frame_2):
+    while True:
+        draw_frame(canvas, row, column, frame_1)
+        await asyncio.sleep(0)
+        draw_frame(canvas, row, column, frame_1, negative=True)
+
+        draw_frame(canvas, row, column, frame_2)
+        await asyncio.sleep(0)
+        draw_frame(canvas, row, column, frame_2, negative=True)
+
+
 def draw(canvas):
     curses.curs_set(0)
     canvas.border()
 
+    with open("animation/rocket_frame_1.txt") as f:
+        rocket_frame_1 = f.read()
+    with open("animation/rocket_frame_2.txt") as f:
+        rocket_frame_2 = f.read()
+
     height, width = canvas.getmaxyx()
+    frame_rows, frame_columns = get_frame_size(rocket_frame_1)
+    row = height // 2 - frame_rows // 2
+    col = width // 2 - frame_columns // 2
 
     coords = [
         (random.randint(1, height - 2), random.randint(1, width - 2))
@@ -69,6 +90,7 @@ def draw(canvas):
 
     coroutines = [blink(canvas, row, col, random.choice("+*.:")) for row, col in coords]
     coroutines.append(fire(canvas, height - 2, width // 2))
+    coroutines.append(animate_rocket(canvas, row, col, rocket_frame_1, rocket_frame_2))
 
     for coro in coroutines:
         for _ in range(random.randint(0, 30)):
